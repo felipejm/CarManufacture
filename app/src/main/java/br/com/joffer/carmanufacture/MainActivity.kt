ebf24bf3d79b2af21f88f0aac05a3bc06d725561
Felipe J Martins <felipe.martins@goin.com.br>
2019-04-08 10:25:44 -0300

    first commit

---
+++ app/src/main/java/br/com/joffer/carmanufacture/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.joffer.carmanufacture
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
